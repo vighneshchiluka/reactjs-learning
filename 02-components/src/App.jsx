835e0e1af5eab67d46ed23c6310e180eedffa4d8
vighnesh chiluka <vighnesh.chiluka25@gmail.com>
2026-07-16 00:37:53 +0530

    Created rafce card components

--- 02-components/src/App.jsx
+++ 02-components/src/App.jsx
@@ -4,12 +4,7 @@ import Card from './components/Card'
 const App = () => {
   return (
     <div>
-      <div className='card'>
-        <h1>Vighnesh Chiluka</h1>
-        <p>Lorem ipsum, dolor sit amet consectetur adipisicing.</p>
-      </div>
-
-      {Card()}
+      <Card />
     </div>
   )
 }
--- 02-components/src/components/Card.jsx
+++ 02-components/src/components/Card.jsx
@@ -1,5 +1,20 @@
-function Card(){
-    return "Hi IM Card Vighnesh Chiluka"
+import React from 'react'
+
+const Card = () => {
+
+    const user = 'Vighnesh Chiluka'
+    const age = 25
+    const profession = 'Front End Developer'
+
+    return (
+        <div>
+            <div className='card'>
+                <h1>Hello, Guys I'm {user}</h1>
+                <h2>And I'm {age} years old</h2>
+                <h3>I'm {profession}</h3>
+            </div>
+        </div>
+    )
 }
 
 export default Card
